1616274b59da5870d940f58a8710c2203a26523e
leosign <leonardo.signorini@studio.unibo.it>
2023-03-13 12:02:33 -0400

    vecchi prototipi dei nemici e gestore della lista delle stanze. Implementazione delle stanze individuali in corso

---
+++ room.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+class Room{
+	public:
+		Room(Room *prev);  //nel costruttore next non serve perche' creo le stanze serve in fondo alla lista
+		Room *goprev(Room *r);
+		Room *gonext(Room *r);
+		void initRooms();
+		void generateRoom();
+	private:
+		Room *prev;
+		Room *next;
+};
+
+Room::Room(Room *previous){
+previous->next = this;
+prev = previous;
+next = NULL;
+}
+
+Room *currentRoom;
+
+Room*  Room::goprev(r){
+if (r->prev != NULL) currentRoom = r->prev;
+return currentRoom; 
+}
+
+Room* Room::gonext(r){
+if (r->next != NULL) currentRoom = r->next;
+return currentRoom;
+}
+
+void Room::initRooms(){
+	Room* head_room = new Room();
+	head_room->prev = NULL; 
+	currentRoom = head_room;                //probabile che la prima stanza sia speciale (market)
+}
+
+void generateRoom(){
+srand(time(NULL));
+rand()%20+1;          //19 stanze + 1 market per ora;
+// qui devo capire come salvare e caricare le stanze
+}
+
+
+
+WINDOW definedLevels[20];
+
+
+
