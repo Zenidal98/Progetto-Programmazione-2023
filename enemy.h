a5191a487c1112c47a9a1ccaeb29f71667a5ee69
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-03-30 17:41:21 +0200

    Update and rename Struttura Nemico Base to enemy.h

---
+++ enemy.h
@@ -0,0 +1,30 @@
+#ifndef enemy.h
+#define enemy.h
+
+class enemy {
+private:
+        int x;
+        int y;
+        int speed;
+        int health;
+        int damage;
+
+public:
+    enemy(int x, int y, int speed, int health, int damage);
+
+    int getX();
+
+    int getY();
+
+    int getHealth();
+
+    void takeDamage(int damage);
+
+    void increasehealth(int plush);
+
+    void increasedamage(int plusd);
+
+    void movement();
+
+};
+#endif
