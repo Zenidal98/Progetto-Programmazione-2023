ce5569e9f86ced191d54284d8c5aef35cdfb2a34
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-03 16:52:15 +0200

    Update elements.cpp

--- elements.cpp
+++ elements.cpp
@@ -1,23 +1,38 @@
   #include elements.h         //servono 4 sprite
 
-elements::elements(int value){
+elements::elements(int value, int x, int y, char type){       //i tipi sono d, h, c, r, ossia drink,hole,cherry,rock
     this->value=value;
+    this->x=x;
+    this->y=y;
+    this->type=type;
 }
 
+void touch(){
+    if((player.x==elements.x)&&(player.y==elements.y)){
+        if(elements.char=='d'){                         //check sul tipo di collezionabile raccolto
+            elements::health_up(value);
+        } else if(elements.char=='h'){
+            elements::health_down(value);
+        } else if(elements.char=='c'){
+            elements::score_up(value)
+        } else if(elements.char=='r'){
+            elements::score_down(value);
+        }
+        mvaddch(WINDOW*map,y,x,' ');             //libero quel pezzetto. Forse non basta o crea problemi, da tenere in considerazione
+    }
 
-
-int elements::health_up(int value){
+int elements::health_up(int value){           //drink
     player.hp = player.hp + value;
 }
 
-int elements::health_down(int value){
+int elements::health_down(int value){        //hole
     player.hp = player.hp - value;
 }
 
-int elements::score_up(int value){
+int elements::score_up(int value){           //cherry
     player.hp = player.score + value;
 }
 
-int elements::score_down(int value){
+int elements::score_down(int value){         //rock
     player.hp = player.score - value;
 }
