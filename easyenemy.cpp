2ec0d54fe837a67465f83b2e78ccbd109376f9b9
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-06 20:29:25 +0200

    Update and rename enemy1.cpp to easyenemy.cpp

--- easyenemy.cpp
+++ easyenemy.cpp
@@ -1,5 +1,5 @@
- #include enemy1.h
-       enemy1::enemy1(int x, int y, int speed) {
+ #include easyenemy.h
+       easyenemy::enemy1(int x, int y, int speed) {
         this->x = x;
         this->y = y;
         this->speed = speed;
