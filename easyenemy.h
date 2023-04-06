ae5dfa84a5ba02de8c0b4ea1f8b3565760a48bbf
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-06 20:32:45 +0200

    Update easyenemy.h

--- easyenemy.h
+++ easyenemy.h
@@ -7,7 +7,7 @@ int health=10;
 int damage=3;
 
 public:
-easyenemy(int x, int y, int speed)::enemy(x,y,speed);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
+easyenemy(int x, int y, int speed)::easyenemy(x,y,speed);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
 
 };
 #endif
