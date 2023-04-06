076d2c9392be57f8bc90f4651d30158f92db48c5
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-06 20:27:50 +0200

    Update enemy1.h
    Nemico facile. Vorrei lasciarlo colorato di bianco, a differenza degli altri 2.

--- enemy1.h
+++ enemy1.h
@@ -1,13 +1,13 @@
-#ifndef enemy1.h
-#define enemy1.h
+#ifndef easyenemy.h
+#define easyenemy.h
 
-class enemy1: public enemy {
+class easyenemy: public enemy {
 private:
 int health=10;
 int damage=3;
 
 public:
-enemy1(int x, int y, int speed)::enemy(x,y,speed);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
+easyenemy(int x, int y, int speed)::enemy(x,y,speed);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
 
 };
 #endif
