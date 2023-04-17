ff43293da98804e91ba91acb2d00b0062f63fd41
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-17 18:37:50 +0200

    Update specialpowers.h

--- specialpowers.h
+++ specialpowers.h
@@ -1,1 +1,23 @@
+#ifndef specialpowers.h
+#define specialpowers.h
+
+class specialpowers{                     // classe usata per definire poteri speciali. Il player può comprarne uno nel market, può
+                                         // tenerne solo uno e può attivarlo con un tasto dedicato.
+private:
+   int x;
+   int y;
+   char type;
+
+public:
+   specialpowers(int x, int y, char type);
+
+   void touch();                   //funzione che si attiva sempre quando il player tocca un elemento.
+   
+   void bomb();                    //uccide tutti i nemici in una stanza
+
+   void damageup();                //aumenta il danno bonus
+
+}
+
+#endif specialpowers.h
 
