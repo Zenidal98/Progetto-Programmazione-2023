f0af33b0ef373b5621b712b0f22d5190daf4301e
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-04-26 10:53:09 +0200

    Create easyenemy.h

---
+++ easyenemy.h
@@ -0,0 +1,14 @@
+
+#ifndef easyenemy.h
+#define easyenemy.h
+
+class easyenemy: public enemy {
+private:
+int health=10;
+int damage=3;
+
+public:
+easyenemy(int x, int y, int speed)::easyenemy(x,y,speed);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
+
+};
+#endif
