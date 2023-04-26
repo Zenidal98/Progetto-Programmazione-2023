7a296e1eeb9c0b7633693927c5147e015c0c41de
Lupss19 <117817011+Lupss19@users.noreply.github.com>
2023-04-26 10:16:51 +0200

    Added Jump

--- player.cpp
+++ player.cpp
@@ -1,6 +1,7 @@
-#include "player.hpp"
+#include "player.h"
 
-Player::Player(WINDOW *win, int y, int x, char c, int h=100, int s=0, int d=1, int p=1){
+Player::Player(WINDOW *win, int y, int x, char c, int h=100, int s=0, int jf=1, int jh=1, int d=1, int p=1, int bd=1, int pd=0, int m=0){
+// Player::Player(WINDOW *win, int y, int x, char c){
     curwin = win;
     yLoc = y;
     xLoc = x;
@@ -11,6 +12,11 @@ Player::Player(WINDOW *win, int y, int x, char c, int h=100, int s=0, int d=1, i
     keypad(curwin, true);               //cattura input da tastiera
     health = h;
     score = s;
+    jumpForce = jf;
+    jumpHeight = jh;
+    baseDamage = bd;
+    plusDamage = pd;
+    coins = m;
 }
 
 void Player::plusScore(int points){
@@ -27,6 +33,31 @@ void Player::minusHealth(int hp){
     if(health = 0)
         gameOver = true;
 }*/
+
+void Player::jump(){
+    int i, right;
+    // evitare che wgetch blocchi il salto
+    nodelay(curwin, true);
+    for(i=0; i<jumpHeight; i++){
+        mvup();
+        display();
+        wrefresh(curwin);
+        napms(150);
+        // spostarsi in volo
+        right = wgetch(curwin);
+        if(right == KEY_RIGHT){
+            mvright();
+            display();
+            wrefresh(curwin);
+        }
+    }
+    for(i=0; i<jumpHeight; i++){
+        mvdown();
+        display();
+        wrefresh(curwin);
+        napms(150);
+    }
+}
 
 void Player::mvup(){
     mvwaddch(curwin, yLoc, xLoc, ' ');
@@ -60,7 +91,7 @@ int Player::getmv(){
     int choice = wgetch(curwin);
     switch(choice){
         case KEY_UP:
-            mvup();
+            jump();
             break;
         case KEY_DOWN:
             mvdown();
---
+++ player.h
@@ -0,0 +1,48 @@
+// dove mettere gameover?
+#pragma once
+#include <ncurses.h>
+
+class Player{
+    private:
+        int xLoc, yLoc, xMax, yMax;
+        int health;
+        int score;
+        char character;
+        WINDOW *curwin;
+        // opzioni per salto
+        int jumpForce;
+        int jumpHeight;
+        // distanza di attacco
+        int distance;
+        // potenza di attacco
+        int power; // da togliere?
+        // soldi
+        int coins;
+        // aggiunta
+        int baseDamage;
+        int plusDamage;
+    public:
+        Player(WINDOW *win, int x, int y, char c, int h, int s, int jf, int jh, int d, int p, int bd, int pd, int m);
+        // Player(WINDOW *win, int x, int y, char c);
+        // colpo subito
+        void registerHit();
+        void plusScore(int points);
+        void plusHealth(int hp);
+        void minusHealth(int hp);
+        void jump();
+        void mvup();
+        void mvdown();
+        void mvleft();
+        void mvright();
+        int getmv();
+        void display();
+        // aumenta distanza di attacco 
+        void distanceUp(int plusD);
+        // aumenta potenza di attacco
+        void powerUp(int plusP);
+        // nemico colpito
+        void hitEnemy();
+        // gestione soldi
+        void plusMoney(int plusM);
+        void minusMoney(int minusM);
+};
