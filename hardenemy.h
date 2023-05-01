d9c1cf51e4a36d3a69847bebf9e3c0ea3b8ff3b4
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-05-01 11:24:22 +0200

    Create hardenemy.h

---
+++ hardenemy.h
@@ -0,0 +1,13 @@
+#ifndef hardenemy.h
+#define hardenemy.h
+
+class hardenemy: public enemy {
+private:
+int health=30;
+int damage=15;
+
+public:
+hardenemy(int x, int y, char #)::enemy(x,y,#);
+
+};
+#endif
