60aa59af7f29021a858a2ea83d2375e07a35e4b1
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-05-01 17:08:55 +0200

    Update elements.h

--- elements.h
+++ elements.h
@@ -1,7 +1,7 @@
 #ifndef elements.h
 #define elements.h
 
-class elements{                     // classe usata per definire 2 buff del gioco (ciliegia, bibita) e 2 debuff (sasso, buco)
+class elements{                     // classe usata per definire 3 buff del gioco (ciliegia, bibita, oro) e un debuff (sasso)
 private:
    int x;
    int y;
@@ -17,8 +17,9 @@ public:
    void score_up();           //ciliegia
 
    void score_down();         //sasso
-       
-                             //manca un'ultima funzione per evitare che i buff/debuff spawnino sui bordi della finestra o sulle piattaforme. O forse non serve ?
+     
+   void coins_up();           //oro
+                            
    void elements_display();
 
 }
