e16e7ab922a434a45399a7efa2383023b417510b
Signo <117813578+leosign@users.noreply.github.com>
2023-05-01 06:22:54 -0400

    Update maintest.cpp e update funzioni di spawn

--- maintest.cpp
+++ maintest.cpp
@@ -18,31 +18,96 @@ void teleport(){
 	Player::display();
 }
 
-
 void goNextRoom(pRL oldStage){
 pRL newStage = oldStage->next;
 room::generateRoom(newStage);
 teleport();
+enemySpawn(newStage);	
+elementSpawn(newStage);
 }
+
+char randomizeElementType(){
+	int i =rand()%2;
+	char a;
+	switch(i){
+	case 0:
+		a = 'd';
+		break;
+	case 1:
+		a = 'c';
+		break;		
+	case 2:
+		a = 'd';
+		break;
+	}
+}
+
+
+void elementSpawn(pRL stage){
+	if(rand()%2==1){
+		char a = randomizeElementType;
+		elements::elements(rand()%29+1,rand()%29+1,a);
+		elements::display();
+	}	
+}
+
 
 void enemySpawn(pRL stage){
 	if(stage->roomID>=2 && stage->roomID<=3){
 		for(int i=0, i<2,i++){
 			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
 			easyenemy::display();
-		}
+		}	
 	}
 	if(stage->roomID>3 && stage->roomID<=5){
 		for(int i=0, i<4,i++){
 			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
 			easyenemy::display();
+		}	
+	}
+	if(stage->roomID>5 && stage->roomID<=10){
+		for(int i=0, i<2,i++){
+			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
+			easyenemy::display();
+		}
+		for(int j=0, j<2, j++){
+			mediumenemy::mediumenemy(rand()%29+1,rand()%29+1);
+			mediumenemy::display();
+		}	
+	}
+	if(stage->roomID>10 && stage->roomID<=15){
+		for(int i=0, i<4, i++){
+			mediumenemy::mediumenemy(rand()%29+1, rand()%29+1);
+			mediumenemy::display();
 		}
 	}
-
-
-
-
-
+	if(stage->roomID>15 && stage->roomID<=20){
+		for(int i=0, i<2, i++){
+			mediumenemy::mediumenemy(rand()%29+1,rand()%29+1);
+			mediumenemy::display();
+		}
+		hardenemy::hardenemy(rand()%29+1,rand()%29+1);
+		hardenemy::hardenemy(rand()%29+1,rand()%29+1);
+	}
+	if(stage->roomID>20 && stage->roomID<=25){
+		for(int i=0, i<2, i++){
+			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
+			hardenemy::display();
+		}
+	}
+	if(stage->roomID>25 && stage->roomID<=30){
+		for(int i=0, i<4, i++){
+			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
+			hardenemy::display();			
+		}	
+	}
+	if(stage->roomID>30){
+		int m = (stage->roomId/10)*2;
+		for(int i=0,i<m,i++){   
+			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
+			hardenemy::display();
+		}
+	}
 }
 
 
@@ -79,6 +144,9 @@ do{
 	Player::getmv();
 }while(Player::xLoc!=30 && Player::yLoc!=1)
 goNextRoom(pippo);
+	
+	
+	
 
 return 0;
 }
