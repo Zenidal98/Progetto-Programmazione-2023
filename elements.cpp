4273fdf3a8e64eaedea0c6f2d02c27b00a6f606b
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-05-01 12:01:38 +0200

    Update elements.cpp

--- elements.cpp
+++ elements.cpp
@@ -21,18 +21,33 @@ void touch(){
         mvaddch(WINDOW*map,y,x,' ');             //libero quel pezzetto. Forse non basta o crea problemi, da tenere in considerazione
     }
 
-void elements::health_up(int value){           //drink. Questo va messo anche nel market.
+void elements::health_up(int value){           //drink. Questo va messo anche nel market.    D
     player.hp = player.hp + value;
 }
 
-void elements::health_down(int value){        //hole
+void elements::health_down(int value){        //hole    H
     player.hp = player.hp - value;
 }
 
-void elements::score_up(int value){           //cherry
+void elements::score_up(int value){           //cherry   C
     player.hp = player.score + value;
 }
 
-void elements::score_down(int value){         //rock
+void elements::score_down(int value){         //rock    R. Il punteggio può andare anche in negativo
     player.hp = player.score - value;
+}
+  
+void elements::display(){
+  if(elements.type=='c'){
+  mvwaddch(curwin,y,x,'C')
+    }
+   else if(elements.type=='r'){
+  mvwaddch(curwin,y,x,'R')
+     }
+   else if(elements.type=='h'){
+  mvwaddch(curwin,y,x,'H')
+     }
+   else if(elements.type=='d'){
+  mvwaddch(curwin,y,x,'D')
+     }
 }
