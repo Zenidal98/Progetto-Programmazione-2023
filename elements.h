7149cf378de281c7a87b00941356b70d1de91c1a
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-05-01 17:22:13 +0200

    Update elements.h

--- elements.h
+++ elements.h
@@ -1,27 +1,26 @@
 #ifndef elements.h
 #define elements.h
 
-class elements{                     // classe usata per definire 2 buff del gioco (ciliegia, bibita) e 2 debuff (sasso, buco)
+class elements{                     // classe usata per definire 3 buff del gioco (ciliegia, bibita, oro) e un debuff (sasso)
 private:
-   int value;
    int x;
    int y;
    char type;
 
 public:
-   elements(int value, int x, int y, char type);
+   elements(int x, int y, char type);
 
-   void touch();                   //funzione che si attiva sempre quando il player tocca un elemento.
+   void touch();             //funzione che si attiva sempre quando il player tocca un elemento.
    
-   void health_up(value);          //bibita. Va messa anche nel livello market per potersi curare.
+   void health_up();          //bibita. Va messa anche nel livello market per potersi curare.
 
-   void health_down(value);        //buco
+   void score_up();           //ciliegia
 
-   void score_up(value);           //ciliegia
-
-   void score_down(value);         //sasso
-       
-                             //manca un'ultima funzione per evitare che i buff/debuff spawnino sui bordi della finestra o sulle piattaforme. O forse non serve ?
+   void score_down();         //sasso
+     
+   void coins_up();           //oro
+                            
+   void elements_display();
 
 }
 
