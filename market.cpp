ece262e30800ff7332829622f00637ae4fb721ad
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-06-29 12:54:00 +0200

    Update market.cpp

--- market.cpp
+++ market.cpp
@@ -1,5 +1,8 @@
 Note: -Bisogna capire dove va visto che usa un sacco di attributi di classi diverse.
       -Testare se il mvprintw cosi funziona
+#include<elements.h>
+#include<weapons.h>
+#include<player.h>
 
 void showmarket() {
     clear();  // Cancella lo schermo
@@ -14,7 +17,7 @@ void showmarket() {
     // Per la vita è semplice. Il player ha vita max di 100 quindi un numero casuale fra 1 e 50 va bene.
     int casualhealth=rand%(50)+1;
 
-     // Per i poteri la probabilità è equivalente quindi torna ad essere un pò più semplice
+     // Per i poteri la probabilità che appaia come nome è equivalente 
     int casualindex=rand%(3);
     char powername='.';
     if(casualindex==0){
@@ -30,9 +33,9 @@ void showmarket() {
     char weapons
 
     // Mostra il relativo pulsante da premere per acquistare qualcosa che corrisponde alla iniziale in inglese
-    mvwprintw(healthWin, 1, 1, "Health: " casualhealth " H ");
-    mvwprintw(powerWin, 1, 1, "Power: " powername " P ");
-    mvwprintw(weaponWin, 1, 1, "Weapon: " casualweapon " W ");
+    mvwprintw(healthWin, 1, 1, "Health: " +  casualhealth + " H ");
+    mvwprintw(powerWin, 1, 1, "Power: " + powername + " P ");
+    mvwprintw(weaponWin, 1, 1, "Weapon: " + casualweapon +" W ");
     
     // Aggiorna le finestre
     wrefresh(healthWin);
@@ -42,17 +45,21 @@ void showmarket() {
     // Scelta
     char ch = getch();
     switch (ch) {
-        case 'h':                       // Compra la vita. Se non è al massimo la fa comprare altrimenti niente.
-   
-          
+        case 'h':                       // Compra la vita. Se non è al massimo la fa comprare e se è più di 100 torna a 100 (il massimo)
+        if(player.health<100){
+              player.health=player.health+casualhealth;
+              if(player.health>100){
+                    player.health=100;
+              }
+        }
             break;
-        case 'p':                       // Compra il potere se non ne ha già uno.
+        case 'p':                       // Compra il potere se non ne ha già uno
 
+      //assegna il potere al player. Troppe cose da decidere per scriverlo adesso
           
             break;
         case 'w':                       // Compra l'arma che va a sostituire il potere base del player
-                               
-           
+        weapons::buyweapons(casualweapon);                
             break;
         default:
             break;
