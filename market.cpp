1dc9735e2cc07bacc174acf746982b8560cfff1a
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-06-29 20:12:49 +0200

    Update market.cpp

--- market.cpp
+++ market.cpp
@@ -1,5 +1,6 @@
 Note: -Bisogna capire dove va visto che usa un sacco di attributi di classi diverse.
       -Testare se il mvprintw cosi funziona
+      - Manca andare avanti dopo che si esegue azione associata a tasto
 #include<elements.h>
 #include<weapons.h>
 #include<player.h>
@@ -19,23 +20,38 @@ void showmarket() {
 
      // Per i poteri la probabilità che appaia come nome è equivalente 
     int casualindex=rand%(3);
-    char powername='.';
+    char powername='.';      // Qui metto due variabili per un motivo; la prima serve a far apparire il nome a schermo. La seconda a chiamare
+    char powername2='.';     // la variabile nelle funzioni dedicate nello switch finale, per questo sono solo lettere singole in power2.
     if(casualindex==0){
         powername="Bomb";      
+        powername2='b';
     } else if (casualindex==1){
-        powername="DamageUp";    
+        powername="DamageUp";
+        powername2='d';
     } else if (casualindex==2){
-        powername="........";   
+        powername="........";
+        powername2='.'
     }
     
-    // Per le armi è leggermente più complicato. Ho messo che nel market la pistola appare nel 50% dei casi, il fucile nel 25% ed il
-    // lanciarazzi nel 10% dei casi essendo il più forte.
-    char weapons
+    // La pistola appare nel 50% dei casi, il fucile nel 40% ed il lanciarazzi nel 10% dei casi.
+    int casualindex2=rand%(10)
+    char weaponname='.';
+    char weaponname2='.'
+    if((casualindex2>=0)&&(casualindex2<=4)){
+        weaponname="Pistol";
+        weaponname2='p';
+    } else if ((casualindex2>=5)&&(casualindex2<=8)){
+        weaponname="Firelock";
+        weaponname2='f';
+    } else if (casualindex2==9){
+        weaponname="Rocket_Launcher";
+        weaponname2='l';
+    }
 
     // Mostra il relativo pulsante da premere per acquistare qualcosa che corrisponde alla iniziale in inglese
     mvwprintw(healthWin, 1, 1, "Health: " +  casualhealth + " H ");
     mvwprintw(powerWin, 1, 1, "Power: " + powername + " P ");
-    mvwprintw(weaponWin, 1, 1, "Weapon: " + casualweapon +" W ");
+    mvwprintw(weaponWin, 1, 1, "Weapon: " + weaponname +" W ");
     
     // Aggiorna le finestre
     wrefresh(healthWin);
@@ -59,7 +75,7 @@ void showmarket() {
           
             break;
         case 'w':                       // Compra l'arma che va a sostituire il potere base del player
-        weapons::buyweapons(casualweapon);                
+        weapons::buyweapons(weaponname2);                
             break;
         default:
             break;
