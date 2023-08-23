fe86983a23ec2c7bf425fa3800d374d0d0b9932c
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-08-23 17:54:37 +0200

    Update enemy.hpp

--- enemy.hpp
+++ enemy.hpp
@@ -38,4 +38,36 @@ public:
 
 };
 
+class easyenemy: public enemy {
+private:
+int health=10;
+int damage=3;
+
+public:
+easyenemy(int x, int y, char #)::easyenemy(x,y,#);  //dubbio sul come si strutturino le sottoclassi e i loro metodi im n .h e .cpp
+
+};
+
+
+class mediumenemy: public enemy {
+private:
+int health=20;
+int damage=6;
+
+public:
+mediumenemy(int x, int y, char #)::enemy(x,y,#);
+
+};
+
+
+class hardenemy: public enemy {
+private:
+int health=30;
+int damage=15;
+
+public:
+hardenemy(int x, int y, char #)::enemy(x,y,#);
+
+};
+
 
