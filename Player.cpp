2275d16d6cab37da8538d8efd4853d58320b680f
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-08-30 12:01:12 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,6 @@
 #include "player.hpp"
 
-player::layer(WINDOW *win, int y, int x, char c, int h=100, int s=0, int jf=1, int jh=1, int d=1, int p=1, int bd=1, int pd=0, int m=0, char sp='\0'){
+Player::Player(WINDOW *win, int y, int x, char c, int h=100, int s=0, int jf=1, int jh=1, int d=1, int p=1, int bd=1, int pd=0, int m=0, char sp='\0'){
 // Player::Player(WINDOW *win, int y, int x, char c){
     curwin = win;
     yLoc = y;
