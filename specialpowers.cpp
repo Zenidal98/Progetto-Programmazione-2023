f4b6bfdcf35198fe00d2d48b6a85cfe537fabdf1
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-08-30 11:43:31 +0200

    Update specialpowers.cpp

--- specialpowers.cpp
+++ specialpowers.cpp
@@ -1,12 +1,12 @@
   #include "specialpowers.hpp"
   #include "enemy.hpp"            //serve per takedamage
   #include "Logics.hpp"
-specialpowers::specialpowers(int cost, char type){       //i tipi sono d,t,i
+Specialpowers::Specialpowers(int cost, char type){       //i tipi sono d,t,i
     this->cost=cost
     this->type=type;
 }
 
-void specialpowers::buyspecialpowers(char type){
+void Specialpowers::buyspecialpowers(char type){
     if(player.gold >= this->cost){                    //se si ha abbastanza gold si può comprare
         if(this->type=='d'){                          //comprare la pistola
           player.sp='d';
@@ -18,7 +18,7 @@ void specialpowers::buyspecialpowers(char type){
     }
 }
 
-void poweractivation(){                          //funzione che attiva gli specialpowers. Forse va messa nel main e basta.
+void Specialpowers::poweractivation(){                          //funzione che attiva gli specialpowers. Forse va messa nel main e basta.
 	if (wgetch(curwin)=='x'){
 		if (player.sp='d'){
         	specialpowers::damageup();
@@ -32,19 +32,19 @@ void poweractivation(){                          //funzione che attiva gli speci
 	}
 }
 
-void specialpowers::damageup(){
+void Specialpowers::damageup(){
     player.bonusdamage=player.bonusdamage*1,5;  //aumenta il danno bonus del player. Questo sommato al danno arma è il danno totale.
       }
 
-void specialpowers::damagedown(){                //legata a damagedown
+void Specialpowers::damagedown(){                //legata a damagedown
     player.bonusdamage=player.bonusdamage/1,5;
       }
 
-void specialpowers::teleport(){                   //teletrasporta il giocatore in una nuova stanza
+void Specialpowers::teleport(){                   //teletrasporta il giocatore in una nuova stanza
 	Logics::teleportpower();
 }
 
-void specialpowers::invincible(){                //Idea: salvo gli hp che ho al momento di invincible e l'id della stanza
+void Specialpowers::invincible(){                //Idea: salvo gli hp che ho al momento di invincible e l'id della stanza
               //dell'attivazione del potere. Probabile errore su roomID. 
 //	while(invincibleroomID==roomID){
 //            player.hp=invinciblehp;
