b77d1ed0f5e764326418f7f6d8e797c138d82a1f
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-08-30 11:44:51 +0200

    Update weapons.hpp

--- weapons.hpp
+++ weapons.hpp
@@ -1,14 +1,14 @@
 #ifndef WEAPONS_HPP                               //sono le armi da inserire nel livello market
 #define WEAPONS_HPP
 
-class weapons {
+class Weapons {
 private:
        int cost;
        int plus;
        char type;
 
 public:
-    weapons(int cost, int plus, char type);   //oltre al costo e al buff che danno si potrebbe mettere un diverso modo di sparare
+    Weapons(int cost, int plus, char type);   //oltre al costo e al buff che danno si potrebbe mettere un diverso modo di sparare
 
     void buyweapons(char type);
 
