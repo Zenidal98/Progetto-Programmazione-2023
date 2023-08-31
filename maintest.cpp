72f8a8a60181e71d66567eb2b6599bff5f1c2f8e
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2023-08-31 11:41:27 +0200

    Update maintest.cpp

--- maintest.cpp
+++ maintest.cpp
@@ -82,35 +82,35 @@ void elementSpawn(pRL stage){
 
 void enemySpawn(pRL stage){
 	if(stage->roomID>=2 && stage->roomID<=3){
-		for(int i=0, i<2,i++){
+		for(int i=0; i<2; i++){
 			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
 			easyenemy::display();
 		}	
 	}
 	if(stage->roomID>3 && stage->roomID<=5){
-		for(int i=0, i<4,i++){
+		for(int i=0; i<4; i++){
 			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
 			easyenemy::display();
 		}	
 	}
 	if(stage->roomID>5 && stage->roomID<=10){
-		for(int i=0, i<2,i++){
+		for(int i=0; i<2; i++){
 			easyenemy::easyenemy(rand()%29+1,rand()%29+1);
 			easyenemy::display();
 		}
-		for(int j=0, j<2, j++){
+		for(int j=0; j<2; j++){
 			mediumenemy::mediumenemy(rand()%29+1,rand()%29+1);
 			mediumenemy::display();
 		}	
 	}
 	if(stage->roomID>10 && stage->roomID<=15){
-		for(int i=0, i<4, i++){
+		for(int i=0; i<4; i++){
 			mediumenemy::mediumenemy(rand()%29+1, rand()%29+1);
 			mediumenemy::display();
 		}
 	}
 	if(stage->roomID>15 && stage->roomID<=20){
-		for(int i=0, i<2, i++){
+		for(int i=0; i<2; i++){
 			mediumenemy::mediumenemy(rand()%29+1,rand()%29+1);
 			mediumenemy::display();
 		}
@@ -118,20 +118,20 @@ void enemySpawn(pRL stage){
 		hardenemy::hardenemy(rand()%29+1,rand()%29+1);
 	}
 	if(stage->roomID>20 && stage->roomID<=25){
-		for(int i=0, i<2, i++){
+		for(int i=0; i<2; i++){
 			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
 			hardenemy::display();
 		}
 	}
 	if(stage->roomID>25 && stage->roomID<=30){
-		for(int i=0, i<4, i++){
+		for(int i=0; i<4; i++){
 			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
 			hardenemy::display();			
 		}	
 	}
 	if(stage->roomID>30){
 		int m = (stage->roomId/10)*2;
-		for(int i=0,i<m,i++){   
+		for(int i=0; i<m; i++){   
 			hardenemy::hardenemy(rand()%29+1,rand()%29+1);
 			hardenemy::display();
 		}
